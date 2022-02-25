af15ca42bbc29b7eb1bf5196d4525a2988b90048
m.paolella1@campus.unimib.it <m.paolella1@campus.unimib.it>
2022-02-25 08:05:49 +0100

    Aggiunto nuovo dataset NoAWE

--- cyclomatic_complexity/generate_dataset.py
+++ cyclomatic_complexity/generate_dataset.py
@@ -1,5 +1,7 @@
 import os
 import ast
+import re
+
 import numpy as np
 import json
 from radon.complexity import cc_visit
@@ -10,7 +12,15 @@ date = datetime.now().strftime("%H%M%S_%d%m%y")
 directory = 'code_dataset'
 
 
-def read_data(data_folder):
+def existAWE(function):
+    tmp = re.sub(r"(\"[^\"]*\")|(\'[^\']*\')", "\"template\"", function)
+    if re.findall(r"(except )|(with )|(assert )", tmp):
+        return True
+
+    return False
+
+
+def read_data(data_folder, includeAWE=True):
     data_cc = []
     list_cc = []
     syn_err = 0
@@ -25,11 +35,10 @@ def read_data(data_folder):
             tmp_dict = ast.literal_eval(line.strip())
             if tmp_dict['label'] == "Correct":
                 try:
-                    #print(cc_visit(tmp_dict['function']))
-                    #break
-                    cc = cc_visit(tmp_dict['function'])[0].complexity
-                    list_cc.append(cc)
-                    data_cc.append({"function": tmp_dict['function'], "label": cc})
+                    if includeAWE or existAWE(tmp_dict['function']):
+                        cc = cc_visit(tmp_dict['function'])[0].complexity
+                        list_cc.append(cc)
+                        data_cc.append({"function": tmp_dict['function'], "label": cc})
                 except SyntaxError:
                     syn_err += 1
                     pass
@@ -50,7 +59,7 @@ def save_data(data_cc):
     uni_err_scr = 0
     print("\nSalvataggio dati...")
     os.mkdir('result/' + date)
-    f2 = open('result/' + date + '/data_cc.jsontxt', 'x')
+    f2 = open('result/' + date + '/data_cc_NoAWE_1955.jsontxt', 'x')
     for dt in data_cc:
         try:
             f2.write(json.dumps(dt) + "\n")
@@ -83,8 +92,8 @@ def select_data(data_cc, label=10, quantity=1000, binary=False):
 
 
 def main():
-    data_cc = read_data(directory)
-    data_cc = select_data(data_cc, label=20, quantity=150, binary=True)
+    data_cc = read_data(directory, includeAWE=False)
+    data_cc = select_data(data_cc, label=20, quantity=100, binary=True)
     save_data(data_cc)
 
 
